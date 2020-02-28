4955d237c2bb9b3febc23f8263a49bcb6de8030c
mabu <mahabaleshwar.hegde@thoughtworks.comcd>
2020-02-28 10:39:43 +0530

    - added Music List View Model test

---
+++ CheetaTests/MusicListViewModelTest.swift
@@ -0,0 +1,42 @@
+//
+//  MusicListViewModelTest.swift
+//  CheetaTests
+//
+//  Created by Mahabaleshwar on 28/02/20.
+//  Copyright © 2020 TW. All rights reserved.
+//
+
+import XCTest
+@testable import Cheeta
+
+class MusicListViewModelTest: XCTestCase {
+
+    private var viewModel: MusicListViewModel!
+    
+    override func setUp() {
+        self.viewModel = MusicListViewModel()
+    }
+
+    func testMusicListTestResturnsValidData() {
+        let data = JSONManager.getJSONData(fileName: "MusicRequestSearchResponse")
+        viewModel.networkManager = getMockedNetworkManager(data: data, error: nil)
+        viewModel.fetchMusicList(searchTerm: "hello") { (response) in
+            XCTAssertNotNil(response, "Music List response returns nil")
+        }
+    }
+    
+    func testMusicListTestResturnsNilResponseWhenDataIsNilOrError() {
+        viewModel.networkManager = getMockedNetworkManager(data: nil, error: HTTPStatusCode.noContent)
+        viewModel.fetchMusicList(searchTerm: "hello") { (response) in
+            XCTAssertNil(response, "Music List response not nil")
+        }
+    }
+    
+    
+    private func getMockedNetworkManager(data: Data?, error: Error?) -> NetworkManagerInitializable {
+        let session = MockedURLSession()
+        session.data = data
+        let networkManager = MockNetworkManager(session: session)
+        return networkManager
+    }
+}
