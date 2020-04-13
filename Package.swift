fa3994557b78aefec9fb3c1ba2cdff701d9eeb44
mabu <mahabaleshwar.hegde@thoughtworks.comcd>
2020-04-13 13:53:36 +0530

    - updated package

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "Cheeta", dependencies: [], exclude: ["Example"]),
+        .target(name: "Cheeta", dependencies: [], exclude: ["Example", "Tests"]),
         .testTarget(
             name: "CheetaTests",
             dependencies: ["Cheeta"]),
